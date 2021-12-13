f018790cf9793b2eed86db1c0983e3fbf1fb3088
yury trypolski <yury101.89@gmail.com>
2021-12-14 00:35:51 +0300

    added functional by showing specializations in categories

--- force-app/main/default/lwc/category/category.js
+++ force-app/main/default/lwc/category/category.js
@@ -1,13 +1,24 @@
 import { LightningElement, wire } from 'lwc';
 import getCategories from '@salesforce/apex/CategoryController.getCategories';
+import getSpecializatios from '@salesforce/apex/SpecializationController.getSpecializatios';
 
 export default class Category extends LightningElement {
     categories;
+    // specializatios;
 
     connectedCallback() {
         getCategories()
             .then((result) => {
                 this.categories = result;
+                console.log('result ', result);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+            getSpecializatios()
+            .then((result) => {
+                this.specializatios = result;
             })
             .catch((error) => {
                 console.log(error);
