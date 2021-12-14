591022741c75cbabaf454147d9135092bc66cc44
yury trypolski <yury101.89@gmail.com>
2021-12-14 15:14:59 +0300

    added functional by hiding component Category on click

--- force-app/main/default/lwc/category/category.js
+++ force-app/main/default/lwc/category/category.js
@@ -1,27 +1,30 @@
 import { LightningElement, wire } from 'lwc';
 import getCategories from '@salesforce/apex/CategoryController.getCategories';
-import getSpecializatios from '@salesforce/apex/SpecializationController.getSpecializatios';
+// import getSpecializatios from '@salesforce/apex/SpecializationController.getSpecializatios';
 
 export default class Category extends LightningElement {
     categories;
     // specializatios;
 
+    showSpecialist(event) {
+        this.template.querySelector('.containerCategory').style = 'display:none';
+    }
+
     connectedCallback() {
         getCategories()
             .then((result) => {
                 this.categories = result;
-                console.log('result ', result);
             })
             .catch((error) => {
                 console.log(error);
             });
 
-            getSpecializatios()
-            .then((result) => {
-                this.specializatios = result;
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+            // getSpecializatios()
+            // .then((result) => {
+            //     this.specializatios = result;
+            // })
+            // .catch((error) => {
+            //     console.log(error);
+            // });
     }
 }
