81d3017b7c36fbbd1173203afd4e942500609c9c
yury trypolski <yury101.89@gmail.com>
2021-12-16 17:21:37 +0300

    added functional dynamyc output specialists in specialization

--- force-app/main/default/lwc/specialist/specialist.js
+++ force-app/main/default/lwc/specialist/specialist.js
@@ -1,6 +1,37 @@
 import { LightningElement, api } from 'lwc';
+import getSpecialists from '@salesforce/apex/SpecialistController.getSpecialists';
 
 export default class Specialist extends LightningElement {
     @api categories;
     @api specializationName;
+    specialistsForSpecialization;
+    specialists;
+    
+
+    connectedCallback() {
+        getSpecialists()
+        .then((result) => {
+            this.specialists = result;
+            this.staffSpecialists();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+        
+    }
+
+    staffSpecialists() {
+        let array = new Array();
+        this.specialists.forEach(el => {
+            if (el.Specialization__r.Name === this.specializationName) {
+                array.push({
+                  'id':el.Id,
+                  'lastName':el.Last_Name__c,
+                  'firstName':el.First_Name__c
+              })
+            }
+        });
+        this.specialistsForSpecialization = array;
+    }
 }
